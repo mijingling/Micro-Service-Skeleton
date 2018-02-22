5c55a267dd7968d613eb15bc6af49f23fe451b18
ningwa <695968866@qq.com>
2018-02-23 00:24:41 +0800

    本地服务配置参数调整

--- auth-center/src/main/java/com/microservice/skeleton/auth/AuthCenterApplication.java
+++ auth-center/src/main/java/com/microservice/skeleton/auth/AuthCenterApplication.java
@@ -3,7 +3,6 @@ package com.microservice.skeleton.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
